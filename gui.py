1e789cf403c1ffe48ba60decd4174da36e8aa27b
Indra Wahyu <spot@tracert.id>
2022-09-20 23:22:43 +0800

    clean code

--- gui.py
+++ gui.py
@@ -1,36 +1,103 @@
-import tkinter as tk
-from tkinter import *
-from tkinter import filedialog
-from tkinter.filedialog import askopenfile
-from PIL import Image, ImageTk
 
-my_w = tk.Tk()
-my_w.geometry("410x300")  # Size of the window 
-my_w.title('www.plus2net.com')
-my_font1=('times', 18, 'bold')
-l1 = tk.Label(my_w,text='Upload Files & display',width=30,font=my_font1)  
-l1.grid(row=1,column=1,columnspan=4)
-b1 = tk.Button(my_w, text='Upload Files', 
-     width=20,command = lambda:upload_file())
-b1.grid(row=2,column=1,columnspan=4)
+import PySimpleGUI as sg
+import cv2
 
-def upload_file():
-     f_types = [('Jpg Files', '*.jpeg'),
-     ('PNG Files','*.png')]   # type of files to select 
-     filename = tk.filedialog.askopenfilename(multiple=True,filetypes=f_types)
-     col=1 # start from column 1
-     row=3 # start from row 3 
-     for f in filename:
-          img=Image.open(f) # read the image file
-          img=img.resize((100,100)) # new width & height
-          img=ImageTk.PhotoImage(img)
-          e1 =tk.Label(my_w)
-          e1.grid(row=row,column=col)
-          e1.image = img
-          e1['image']=img # garbage collection 
-          if(col==3): # start new line after third column
-               row=row+1# start wtih next row
-               col=1    # start with first column
-          else:       # within the same row 
-               col=col+1 # increase to next column                 
-my_w.mainloop()  # Keep the window open
+sg.theme('dark grey 9')
+
+
+window = sg.Window('Window Title')
+
+
+filename = sg.popup_get_file('Enter the file you wish to process')
+
+print(filename)
+
+
+img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+
+dimensions = img.shape
+
+
+height = img.shape[0]
+width = img.shape[1]
+channels = img.shape[2]
+
+var4 = channels ** 2 # rumus nya 2/4 = 16
+
+var = str(width)
+var2 = str (height)
+var3 = str("ukuran")
+
+print('Image Dimension    : ',var3,var,"X",var2)
+print('Image Height       : ',height)
+print('Image Width        : ',width)
+print('Number of Channels : ',channels)
+print('color depth        : ',var4)
+
+
+mytex = "Image Dimension",var3,var, "X", var2
+varheit = "Image Height",height
+varwidit = "Image Width",width
+varcanel = "Number of Channels",channels
+var444 = "color depth",var4
+
+
+
+
+#mylist = ['Image Dimension    : ',mytex,
+#'Image Height       : ',height,
+#'Image Width        : ',width,
+#'Number of Channels : ',channels,
+#'color depth        : ',var4
+
+#]
+
+layout = [
+            [sg.Text(mytex)],
+            [sg.Text(varheit)],
+            [sg.Text(varwidit)],
+            [sg.Text(varcanel)],
+            [sg.Text(var444)],
+            [sg.Button('Save'), sg.Button('Exit')]
+         ]
+
+window = sg.Window('To Do List Example', layout)
+
+
+  
+# path 
+path = filename
+  
+# Reading an image in default mode
+image = cv2.imread(path)
+  
+# Window name in which image is displayed
+window_name = 'image'
+  
+# Using cv2.imshow() method 
+# Displaying the image 
+resize = cv2.resize(image, (246, 240))
+cv2.imshow(window_name, resize)
+  
+#waits for user to press any key 
+#(this is necessary to avoid Python kernel form crashing)
+event, values = window.read()
+cv2.waitKey(0) 
+  
+#closing all open windows 
+cv2.destroyAllWindows() 
+
+
+
+
+
+
+
+    
+
+#sg.Window('You entered', var3,var, "X", var2)
+
+
+
+
+
